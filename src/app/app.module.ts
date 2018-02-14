0917e353f20ac66aff5e5ee0e449938da982c487
Siddharth Sogani <siddharthsogani22@gmail.com>
2018-02-15 00:10:57 +0530

    First assignment solutions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {DISHES} from '../shared/dishes';
+import {Dish} from '../shared/dish';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+  dishes = DISHES;
+
+  selectedDish;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSelect(dish: Dish) {
+    this.selectedDish = dish;
+  }
+}
