eedf5d041c4927ee5889d532f16f8ad457290d31
Siddharth Sogani <siddharthsogani22@gmail.com>
2018-03-05 10:08:20 +0530

    Angular Testing

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,40 @@
 import { AppPage } from './app.po';
+import {browser} from 'protractor';
 
-describe('my-app App', () => {
+describe('CV da Dhaba App', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  it('should display message saying CV da Dhaba', () => {
+    page.navigateTo('/');
+    expect(page.getParagraphText('app-header .navbar-brand')).toEqual('CV da Dhaba');
+  });
+
+  it('should navigate to about us page by clicking on the link', () => {
+    page.navigateTo('/');
+
+    let navlink = page.getAllElements('a').get(4);
+    navlink.click();
+
+    expect(page.getParagraphText('h3')).toBe('About Us');
+  });
+
+  it('should enter a new comment for the first dish', () => {
+    page.navigateTo('/dishdetail/1');
+
+    setTimeout(function () {
+      const newAuthor = page.getElement('input[type=text]');
+      newAuthor.sendKeys('Siddharth Sogani');
+
+      const newComment = page.getElement('textarea');
+      newComment.sendKeys('Siddharth Sogani liked this dish very very much');
+
+      const newSubmitButton = page.getElement('button[type=submit]');
+      newSubmitButton.click();
+      browser.pause();
+    }, 5000);
   });
 });
--- src/app/menu/menu.component.spec.ts
+++ src/app/menu/menu.component.spec.ts
@@ -1,16 +1,40 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MenuComponent } from './menu.component';
+import {RouterTestingModule} from '@angular/router/testing';
+import {SpinnerComponent} from '../spinner/spinner.component';
+import {HighlightDirective} from '../directives/highlight.directive';
+import {DishService} from '../services/dish.service';
+import {Observable} from 'rxjs/Observable';
+import {Dish} from '../shared/dish';
+import {DISHES} from '../shared/dishes';
+import {baseURL} from '../shared/baseurl';
+import {DebugElement} from '@angular/core';
+import {By} from '@angular/platform-browser';
 
 describe('MenuComponent', () => {
   let component: MenuComponent;
   let fixture: ComponentFixture<MenuComponent>;
 
   beforeEach(async(() => {
+    const dishServiceStub = {
+      getDishes: function(): Observable<Dish[]> {
+        return Observable.of(DISHES);
+      }
+    };
     TestBed.configureTestingModule({
-      declarations: [ MenuComponent ]
+      imports: [
+        RouterTestingModule.withRoutes([{ path: 'menu', component: MenuComponent }])
+      ],
+      declarations: [ MenuComponent, SpinnerComponent, HighlightDirective ],
+      providers: [
+        { provide: DishService, useValue: dishServiceStub },
+        { provide: 'BaseURL', useValue: baseURL },
+      ]
     })
-    .compileComponents();
+      .compileComponents();
+
+    let dishservice = TestBed.get(DishService);
   }));
 
   beforeEach(() => {
@@ -19,7 +43,24 @@ describe('MenuComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('this test case should create a component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('dishes items should be 4', () => {
+    expect(component.dishes.length).toBe(4);
+    expect(component.dishes[1].name).toBe('Pooran Poli');
+    expect(component.dishes[3].featured).toBeFalsy();
+  });
+
+  it('should use dishes in the template', () => {
+    fixture.detectChanges();
+
+    let de:      DebugElement;
+    let el:      HTMLElement;
+    de = fixture.debugElement.query(By.css('h1'));
+    el = de.nativeElement;
+    expect(el.textContent).toContain(DISHES[0].name.toUpperCase());
+
   });
 });
